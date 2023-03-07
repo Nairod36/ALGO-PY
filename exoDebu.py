7f84847a9725af44dafae03d9af23b9886b1d01b
Nairod36 <moulin.solal@gmail.com>
2023-03-07 12:59:11 +0100

    Update exoDebu.py

--- exoDebu.py
+++ exoDebu.py
@@ -51,3 +51,47 @@ import math
 #     print("Ce n'est pas un email valide.")
 
 # # 7
+# for i in range(10):
+#     print("Ceci est un message affiché 10 fois.")
+    
+# # 8
+# mot = input("Entrez un mot : ")
+# for lettre in mot:
+#     print(lettre)
+
+# # 9
+# a = 0
+# b = 10
+# while a < b:
+#     print(a)
+#     a += 1
+
+# # 10
+# b = 10;
+# while b > 0:
+#     b -= 1
+#     if b % 2 == 1:
+#         print(b)
+
+# # 11
+# nombre = 0
+# while nombre < 1 or nombre > 10:
+#     nombre = int(input("Entrez un entier entre 1 et 10 : "))
+# print("Vous avez saisi", nombre)
+
+# # 12
+# chaine = input("Entrez une chaîne de caractères : ")
+# for chatacter in chaine:
+#         print(chatacter)
+# list = input("Entrez une liste de nombres : ")
+# for nombre in list:
+#     print(nombre)
+# for list in range(1,14,3):
+#     print(list)
+
+# # 13
+# nombre = 0
+# list = input("Entrez une liste de nombres : ")
+# while nombre < list:
+#     for :
+#         print(nombre)
