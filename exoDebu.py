e2f410578a63693ec68c1b551c90cdbac37ee1ba
Nairod36 <moulin.solal@gmail.com>
2023-03-07 12:19:37 +0100

    Create exoDebu.py

---
+++ exoDebu.py
@@ -0,0 +1,53 @@
+import math
+
+# # 2
+# nom = input("Entrez votre nom : ")
+# age = input("Entrez votre âge : ")
+# print("Votre nom est : ", nom)
+# print("Votre âge est : ", age)
+# nom = str(nom)
+# age = int(age)
+# print("Votre nom est : ", nom)
+# print("Votre âge est : ", age)
+
+
+# # 3
+# nombre = float(input("Entrez un nombre : "))
+# if nombre >= 0:
+#     racine_carree = math.sqrt(nombre)
+#     print("La racine carrée de", nombre, "est", racine_carree)
+# else:
+#     print("Le nombre saisi doit être positif ou nul.")
+
+# # 4
+# mot1 = input("Entrez le premier mot : ")
+# mot2 = input("Entrez le deuxième mot : ")
+# if mot1 < mot2:
+#     print(mot1, "est le plus petit.")
+# elif mot2 < mot1:
+#     print(mot2, "est le plus petit.")
+# else:
+#     print("Les deux mots sont égaux.")
+
+# # 5
+# pSeuil = 2.3
+# vSeuil = 7.41
+# pression = float(input("Entrez la pression courante de l'enceinte : "))
+# volume = float(input("Entrez le volume courant de l'enceinte : "))
+# if pression > pSeuil and volume > vSeuil:
+#     print("Arrêt immédiat ! La pression et le volume sont supérieurs aux seuils.")
+# elif pression > pSeuil:
+#     print("Augmentez le volume de l'enceinte pour baisser la pression.")
+# elif volume > vSeuil:
+#     print("Diminuez le volume de l'enceinte pour baisser la pression.")
+# else:
+#     print("Tout va bien !")
+
+# # 6
+# chaine = input("Entrez une chaîne de caractères : ")
+# if "@" in chaine and chaine.endswith(".com"):
+#     print("C'est un email valide.")
+# else:
+#     print("Ce n'est pas un email valide.")
+
+# # 7
